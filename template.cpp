c9b04769770032540275974ad9ffbc1841421937
kitoriaaa <69099518+kitoriaaa@users.noreply.github.com>
2021-10-14 23:53:22 +0900

    add coding template file

---
+++ template.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n)        for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n)    for (int i = (s); i < (int)(n); i++)
+#define all(a) a.begin(), a.end()
+#define All(a) a.rbegin(), a.rend()
+typedef vector<int> VI;
+typedef long long int ll;
+
+
+int main() {
+    
+
+    return 0;
+}
---
+++ template.py
@@ -0,0 +1,11 @@
+import sys
+sys.setrecursionlimit(10**9) # 最大再帰数
+MOD = 10**9+7
+
+
+def main():
+    pass
+    
+
+if __name__ == "__main__":
+    main()
